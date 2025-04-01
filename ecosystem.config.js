3580166492f52160afc484be6f149794957f1704
Logan Singleton <logansingleton3@gmail.com>
2025-04-01 15:38:09 -0600

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "date-randomizer-api",
+      script: "./server/index.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+        ALLOWED_ORIGINS:
+          "https://date-randomizer.com,https://www.date-randomizer.com",
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      watch: false,
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+    },
+  ],
+};
